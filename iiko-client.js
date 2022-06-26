aa3f3a925fdfc3c02218accb8b4715a9914c5283
Ignas Račkus <pardavimai@its360.lt>
2022-06-26 17:32:11 +0300

    IIKO client done & tested

--- iiko-client.js
+++ iiko-client.js
@@ -1,8 +1,69 @@
 'use strict'
 
+const axios = require('axios').default;
+
 class IikoClient {
 
-    constructor() {
+    #_api = {
+        address: null,
+        login: null,
+        baseURL: "api/1/"
+    };
+
+    #_client;
+
+    /**
+     *
+     * @param address {String}
+     * @param login {String}
+     */
+
+    constructor(address, login) {
+        this.#_api.address = address;
+        this.#_api.login = login;
+    }
+
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+
+    async connect(){
+        try{
+            let result = await axios.post(`${this.#_api.address}${this.#_api.baseURL}access_token`, {
+                "apiLogin": this.#_api.login
+            });
+            this.#_client = axios.create({
+                baseURL: `${this.#_api.address}${this.#_api.baseURL}`,
+                timeout: 1000,
+                headers: {'Authorization': `Bearer ${result.data.token}`}
+            });
+        }catch (err){
+            throw Error(err);
+        }
+    }
+
+    async request(method, endpoint, query = "", options, retry = true){
+        try{
+            let result = await this.#_client[method](`${endpoint}?${query}`, options);
+            return result.data;
+        }catch (err){
+            if (err.response) {
+                if(retry){
+                    try{
+                        await this.connect();
+                        let result = await this.request(method, endpoint, query = "", options, retry = false);
+                        return result;
+                    }catch (err){
+                        throw Error(err);
+                    }
+                }else{
+                    throw Error(err);
+                }
+            }else{
+                throw Error(err);
+            }
+        }
     }
 
 }
